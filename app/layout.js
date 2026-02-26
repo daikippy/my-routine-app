77109d1a472104cd4d57f250cd05854effb0b7f9
daikippy <fuyuandagui31@gmail.com>
2026-02-26 21:15:54 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <body className="antialiased">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
