c4627b72f767ff68208d02e0d9f46d551caf6531
daikippy <fuyuandagui31@gmail.com>
2026-02-26 21:54:14 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
+      <head>
+        <title>ROUTINE MASTER</title>
+      </head>
       <body>{children}</body>
     </html>
   );
