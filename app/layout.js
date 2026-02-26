ca8e662ef727e716da0a7e539b6c33fcd2cbc7cd
daikippy <fuyuandagui31@gmail.com>
2026-02-26 21:13:56 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,9 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "ROUTINE MASTER",
-  description: "Manage your daily routines",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
